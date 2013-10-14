0cac0c1e2e13e1fdb582ccfd26d71602c745d8c6
Tarek Auel <tarek.auel@googlemail.com>
2013-10-15 01:22:19 +0200

    rename

--- WinnerGame/src/Server/IRoundSensitive.java
+++
@@ -1,9 +0,0 @@
-package Server;
-//Interface fuer Rundensensitive Sachen (Company und BankAccount)
-public interface IRoundSensitive {
-	/**
-	 * Bereitet auf neue Runde vor
-	 * @param round aktuelle Spielrunde
-	 */
-	public void prepareForNewRound(int round)throws Exception;
-}
