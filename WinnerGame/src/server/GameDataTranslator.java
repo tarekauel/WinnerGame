1fdf8247717ae71b9c0a9b0408498aae9fa19370
Michael.Scheid <michischeid@web.de>
2013-10-15 10:42:04 +0200

    Translator angepasst

--- WinnerGame/src/server/GameDataTranslator.java
+++ WinnerGame/src/server/GameDataTranslator.java
@@ -344,7 +344,7 @@ public class GameDataTranslator {
 		for (TMarketShare marketShare : CustomerMarket.getMarket()
 				.getMarketShares()) {
 			marketShares.add(new MarketShareToClient(marketShare
-					.getMarketShare(), company.getName()));
+					.getMarketShare(), marketShare.getCompany().getName()));
 		}
 
 		// getWaferPrices
