2e8e9d91a7ba5c53578565720c1d13423354fc6c
Lars Tilsner <lars.tilsner@googlemail.com>
2013-10-16 21:54:48 +0200

    commit des Produce Tests

--- WinnerGame/tests/productionordern/ProduceTest.java
+++ WinnerGame/tests/productionordern/ProduceTest.java
@@ -40,10 +40,10 @@ public class ProduceTest {
 	@Test
 	public void produceValid() throws Exception{
 		prodOrd = p.getListOfAllProductionOrders().get(0);	
-		int advantage = 1000000;
-		prodOrd.produce(advantage, c.getProduction().getMachine());
+	
+		prodOrd.produce(1.0, c.getProduction().getMachine());
 		assertEquals(true,prodOrd.getPanel()!=null);
-		assertEquals(70,prodOrd.getPanel().getQuality()) ;
+		assertEquals(50,prodOrd.getPanel().getQuality()) ;
 		assertEquals(640050,prodOrd.getPanel().getCosts() );
 		
 	}
@@ -52,8 +52,8 @@ public class ProduceTest {
 	@Test
 	public void produceNewQualityDifferenceInvalid() throws Exception{
 		prodOrd = p.getListOfAllProductionOrders().get(0);
-		int advantage = 100000;
-		prodOrd.produce(advantage, c.getProduction().getMachine());
+	
+		prodOrd.produce(1000000.0, c.getProduction().getMachine());
 		assertEquals(70, prodOrd.getPanel().getQuality());
 	}
 	
@@ -64,15 +64,15 @@ public class ProduceTest {
 			cases = new Resource(90, "Gehäuse", 100000);
 			p.createProductionOrder(wafer, cases, 100);
 			prodOrd = p.getListOfAllProductionOrders().get(p.getListOfAllProductionOrders().size()-1);
-			int advantage = 100000;
-			prodOrd.produce(advantage, c.getProduction().getMachine());
+			
+			prodOrd.produce(1000000.0, c.getProduction().getMachine());
 			assertEquals(100, prodOrd.getPanel().getQuality());
 		}
 	@Test
 	public void produceNewQualityInvalid() throws Exception{
 		prodOrd = p.getListOfAllProductionOrders().get(0);
-		int advantage = 1;
-		prodOrd.produce(advantage, c.getProduction().getMachine());
+		
+		prodOrd.produce(0.001, c.getProduction().getMachine());
 		assertEquals(1, prodOrd.getPanel().getQuality());
 	}
 	//TODO: Um InputParameter zu pruefen fehlt die passende Exception es wird immer null zurueckgeliefert momentan
