9394f84780c048d109201583d7e46486f99df0bb
Lars Tilsner <lars.tilsner@googlemail.com>
2013-10-22 12:38:51 +0200

    update ListOfLosers

--- WinnerGame/tests/gameenginetest/ListOfLosersTest.java
+++ WinnerGame/tests/gameenginetest/ListOfLosersTest.java
@@ -18,26 +18,27 @@ public class ListOfLosersTest {
 		Location.initLocations();
 		Benefit.initBenefits();
 		// füge Companys hinzu
-		GameEngine.getGameEngine().addCompany(
-				new Company(Location.getLocationByCountry("USA"), "Test1"));
-		GameEngine.getGameEngine().addCompany(
-				new Company(Location.getLocationByCountry("USA"), "Test2"));
+
+		new Company(Location.getLocationByCountry("USA"), "Test1");
+
+		new Company(Location.getLocationByCountry("USA"), "Test2");
 	}
 
 	@Test
 	public void listOfLosersTest() throws Exception {
 		// Überlaste die Konten aller Firmen
-		for(int i = 0; i<GameEngine.getGameEngine().getListOfCompanys().size();i++){
+		for (int i = 0; i < GameEngine.getGameEngine().getListOfCompanys()
+				.size(); i++) {
 			GameEngine
-			.getGameEngine()
-			.getListOfCompanys()
-			.get(i)
-			.getBankAccount()
-			.decreaseBalance(
-					Constant.BankAccount.START_CAPITAL
-							+ Constant.BankAccount.MAX_CREDIT + 10);
+					.getGameEngine()
+					.getListOfCompanys()
+					.get(i)
+					.getBankAccount()
+					.decreaseBalance(
+							Constant.BankAccount.START_CAPITAL
+									+ Constant.BankAccount.MAX_CREDIT + 10);
 		}
-			assertEquals("Gameengine Runde: " + 1, GameEngine.getGameEngine()
+		assertEquals("Gameengine Runde: " + 1, GameEngine.getGameEngine()
 				.toString());
 
 	}
