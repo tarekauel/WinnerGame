0622763968b7a0a491f40791c19538d2fb7f11cc
Lars Tilsner <lars.tilsner@googlemail.com>
2013-10-22 11:52:11 +0200

    upload ListOfLosersTest

---
+++ WinnerGame/tests/gameenginetest/ListOfLosersTest.java
@@ -0,0 +1,44 @@
+package gameenginetest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import server.Benefit;
+import server.Company;
+import server.GameEngine;
+import server.Location;
+import constant.Constant;
+
+public class ListOfLosersTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Location.initLocations();
+		Benefit.initBenefits();
+		// füge Companys hinzu
+		GameEngine.getGameEngine().addCompany(
+				new Company(Location.getLocationByCountry("USA"), "Test1"));
+		GameEngine.getGameEngine().addCompany(
+				new Company(Location.getLocationByCountry("USA"), "Test2"));
+	}
+
+	@Test
+	public void listOfLosersTest() throws Exception {
+		// Überlaste die Konten aller Firmen
+		for(int i = 0; i<GameEngine.getGameEngine().getListOfCompanys().size();i++){
+			GameEngine
+			.getGameEngine()
+			.getListOfCompanys()
+			.get(i)
+			.getBankAccount()
+			.decreaseBalance(
+					Constant.BankAccount.START_CAPITAL
+							+ Constant.BankAccount.MAX_CREDIT + 10);
+		}
+			assertEquals("Gameengine Runde: " + 1, GameEngine.getGameEngine()
+				.toString());
+
+	}
+}
