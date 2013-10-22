c22855d96d612e8e0af193ad0c8b964bfd6b8b18
Lars Tilsner <lars.tilsner@googlemail.com>
2013-10-22 12:39:45 +0200

    update

--- WinnerGame/tests/scenarien/GameTest.java
+++ WinnerGame/tests/scenarien/GameTest.java
@@ -24,67 +24,62 @@ import server.GameEngine;
 import server.Location;
 
 public class GameTest {
-	
+
 	@Test
 	public void gameTest() throws Exception {
 		Benefit.initBenefits();
 		Location.initLocations();
-		
+
 		GameEngine game = GameEngine.getGameEngine();
-		
-		game.addCompany( new Company(Location.getLocationByCountry("Deutschland"), "SolarWorld"));
-		game.addCompany( new Company(Location.getLocationByCountry("Deutschland"), "SolarPlus"));
-		
-		ArrayList<GameDataMessageFromClient> messages = new  ArrayList<GameDataMessageFromClient>();
-		
+
+		new Company(Location.getLocationByCountry("Deutschland"), "SolarWorld");
+		new Company(Location.getLocationByCountry("Deutschland"), "SolarPlus");
+
+		ArrayList<GameDataMessageFromClient> messages = new ArrayList<GameDataMessageFromClient>();
+
 		ArrayList<RequestFromClient> requests = new ArrayList<RequestFromClient>();
-		requests.add ( new RequestFromClient("Wafer", 25) );
-		requests.add ( new RequestFromClient("Gehäuse", 40) );
-		
+		requests.add(new RequestFromClient("Wafer", 25));
+		requests.add(new RequestFromClient("Gehäuse", 40));
+
 		ArrayList<AcceptedSupplierOfferFromClient> accepted = new ArrayList<AcceptedSupplierOfferFromClient>();
-		
+
 		ArrayList<ProductionOrderFromClient> proOrder = new ArrayList<ProductionOrderFromClient>();
-		
+
 		ArrayList<OfferFromClient> offerList = new ArrayList<OfferFromClient>();
-		
+
 		ArrayList<BenefitBookingFromClient> bBook = new ArrayList<BenefitBookingFromClient>();
-		
-		messages.add( new GameDataMessageFromClient("SolarWorld", 
-				new PurchaseFromClient( requests, accepted ), 
-				new ProductionFromClient(proOrder), 
-				new DistributionFromClient( offerList ), 
-				false, 
-				new HumanResourcesFromClient( bBook), 
-				10000, 
-				true));
-		
-		messages.add( new GameDataMessageFromClient("SolarPlus", 
-				new PurchaseFromClient( requests, accepted ), 
-				new ProductionFromClient(proOrder), 
-				new DistributionFromClient( offerList ), 
-				false, 
-				new HumanResourcesFromClient( bBook), 
-				10000, 
-				true));
-		
-		ArrayList<GameDataMessageToClient> answer =  game.startNextRound(messages);
-		for( GameDataMessageToClient gm2c : answer) {
-			System.out.println("----------------------------------------------");
+
+		messages.add(new GameDataMessageFromClient("SolarWorld",
+				new PurchaseFromClient(requests, accepted),
+				new ProductionFromClient(proOrder), new DistributionFromClient(
+						offerList), false, new HumanResourcesFromClient(bBook),
+				10000, true));
+
+		messages.add(new GameDataMessageFromClient("SolarPlus",
+				new PurchaseFromClient(requests, accepted),
+				new ProductionFromClient(proOrder), new DistributionFromClient(
+						offerList), false, new HumanResourcesFromClient(bBook),
+				10000, true));
+
+		ArrayList<GameDataMessageToClient> answer = game
+				.startNextRound(messages);
+		for (GameDataMessageToClient gm2c : answer) {
+			System.out
+					.println("----------------------------------------------");
 			System.out.println("Company: " + gm2c.getPlayerName());
 			System.out.println("Purchase");
-			for(RequestToClient req : gm2c.purchase.requests ) {
-				System.out.println("Angebote für folgende Anfrage: " + req.name + "(" + req.quality + ")");
-				for(SupplierOfferToClient off : req.supplierOffers) {
-					System.out.println("Angebot: " + off.name + " (" + off.quality + ") Stueckpreis ");
+			for (RequestToClient req : gm2c.purchase.requests) {
+				System.out.println("Angebote für folgende Anfrage: " + req.name
+						+ "(" + req.quality + ")");
+				for (SupplierOfferToClient off : req.supplierOffers) {
+					System.out.println("Angebot: " + off.name + " ("
+							+ off.quality + ") Stueckpreis ");
 				}
 			}
-				
-			System.out.println("----------------------------------------------");
-		}
-		
-		
-		
-		
-	}
 
+			System.out
+					.println("----------------------------------------------");
+		}
+
+	}
 }
